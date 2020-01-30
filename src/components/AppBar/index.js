06f77c9b1846dbaf3b8c73686fb9c58b6eab5012
WebCheng <webster0808web@gmail.com>
2020-01-30 14:02:18 -0800

    First Commit

---
+++ src/components/AppBar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export function ButtonAppBar() {
+    const classes = useStyles();
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
